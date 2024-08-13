89f4b7d5afa226e8f1347b81b65ff4bb48f76548
AyaneBTC <1564239399@qq.com>
2024-08-13 11:33:57 +0800

    feat: add umi parameters to getTokenMetadataGeneral

--- src/tokens/token.ts
+++ src/tokens/token.ts
@@ -4,16 +4,21 @@ import {
   TOKEN_PROGRAM_ID,
 } from "@solana/spl-token";
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
-import { fetchDigitalAsset, Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import {
+  fetchDigitalAsset,
+  Metadata,
+} from "@metaplex-foundation/mpl-token-metadata";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import type { TokenMetadata } from '@solana/spl-token-metadata';
+import type { TokenMetadata } from "@solana/spl-token-metadata";
+import { Umi } from "@metaplex-foundation/umi";
 
 /**
  * Fetches the general token metadata for a given mint (classic ver. or 2022 ver.).
- * 
+ *
  * @param mint PublicKey of the mint
  * @param programId Program ID of the mint. If not provided, it will be fetched from the mint account.
  * @param connection RPC connection to use. If not provided, it will use the devnet cluster.
+ * @param umi Umi RPC connection to use, only required for 2022 tokens. If not provided, it will be created according to `connection`.
  * @returns General token metadata or null if not found. `TokenMetadata` for 2022 tokens, `Metadata` for classic tokens.
  * @see TokenMetadata
  * @see Metadata
@@ -21,7 +26,8 @@ import type { TokenMetadata } from '@solana/spl-token-metadata';
 export async function getTokenMetadataGeneral(
   mint: PublicKey,
   programId?: PublicKey,
-  connection?: Connection
+  connection?: Connection,
+  umi?: Umi
 ): Promise<Metadata | TokenMetadata | null> {
   if (!connection) {
     connection = new Connection(clusterApiUrl("devnet"), "confirmed");
@@ -40,7 +46,9 @@ export async function getTokenMetadataGeneral(
     return await getTokenMetadata(connection, mint);
   }
   if (programId.equals(TOKEN_PROGRAM_ID)) {
-    const umi = createUmi(connection.rpcEndpoint);
+    if (!umi) {
+      umi = createUmi(connection.rpcEndpoint);
+    }
     return (await fetchDigitalAsset(umi, mint as any)).metadata;
   }
 
